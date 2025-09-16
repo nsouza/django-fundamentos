61a64c6c319f7c6af710c9314f4f04c7e08e3c7d
Nilo Souza <nilo.contatos@gmail.com>
2025-09-16 14:44:56 -0300

    migrante

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
-                        verbose_name="ID",
+                        verbose_name="ID",                    
                     ),
                 ),
                 ("name", models.CharField(max_length=255)),
