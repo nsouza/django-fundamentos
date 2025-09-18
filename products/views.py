cd0b0c93a083ddbd432ba7aa8d2c8ea19b836919
Nilo Souza <nilo.contatos@gmail.com>
2025-09-18 20:51:58 -0300

    Listando produto

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Product
+
 
 def products_list(request):
-    return render(request, 'products/products_list.html')
+    products = Product.objects.all().order_by('created_at')
+    return render(request, 'products/products_list.html', {'products':products})
 
 
