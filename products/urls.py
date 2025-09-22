c6125ef890490fe826603b6c5fbc7d0092e05c66
Nilo Souza <nilo.contatos@gmail.com>
2025-09-21 21:06:20 -0300

    Criando lista de detalhes

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from .import views
 
+app_name = 'products'
 urlpatterns = [
-    path('', views.products_list)
+    path('', views.products_list, name="list"),
+    path('<slug:slug>', views.product_page, name="page")
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from sympy import product
 from .models import Product
 
 
@@ -7,3 +8,6 @@ def products_list(request):
     return render(request, 'products/products_list.html', {'products':products})
 
 
+def product_page(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, 'products/product_page.html', {'product':product})
